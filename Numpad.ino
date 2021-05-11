18e078065ec208b5bd3a5635a6a46544d8c4f15e
dogeThinkpad <dogessbm@gmail.com>
2021-05-11 16:57:32 -0400

    Automatic commit.

--- Numpad.ino
+++ Numpad.ino
@@ -1,37 +1,45 @@
 #include <stdint.h>
-#include <map>
 #define LED			LED_BUILTIN
+#define ROW_NUM		5
+#define COL_NUM		4
 typedef unsigned int	uint;
 
-// enum Col { COL_A=2, COL_B, COL_C, COL_D };
-// enum Row { ROW_1=6, ROW_2, ROW_3, ROW_4, ROW_5 };
+typedef enum {COL_A=2, COL_B, COL_C, COL_D}Col;
+typedef enum {ROW_1=6, ROW_2, ROW_3, ROW_4, ROW_5}Row;
 
-std::map<char, uint> cols = { { 'A', 2 }, { 'B', 3 }, { 'C', 4 }, { 'D', 5 } };
-std::map<char, uint> rows = { { '1', 6 }, { '2', 7 }, { '3', 8 }, { '4', 9 }, { '5', 10 } };
+const static Col colArr[COL_NUM] = {COL_A, COL_B, COL_C, COL_D};
+const static Row rowArr[ROW_NUM] = {ROW_1, ROW_2, ROW_3, ROW_4, ROW_5};
 
+static bool numpadState[COL_NUM][ROW_NUM] = {0};
+static bool numpadStateLast[COL_NUM][ROW_NUM] = {0};
 
-// bool isBtnPressed(const Row row, const Col col) {
-//
-// 	return false;
-// }
+bool pressedRC(const Row row, const Col col)
+{
+
+	return false;
+}
 
 void setup() {
 	pinMode(LED, OUTPUT);
 
-	for (const auto& kv : rows) {
-		pinMode(kv.second, INPUT_PULLUP);
+	for(uint row = 0; row < ROW_NUM; row++){
+		pinMode(rowArr[row], INPUT_PULLUP);
 	}
-	for (const auto& kv : cols) {
-		pinMode(kv.second, INPUT);
+	for(uint col = 0; col < COL_NUM; col++){
+		pinMode(colArr[col], INPUT);
 	}
 }
 
 void loop() {
 	digitalWrite(LED, HIGH);
-	for (const auto& kvRow : rows) {
-		for (const auto& kvCol : cols) {
-			pinMode(kvCol.second, OUTPUT);
-			pinMode(kvCol.second, INPUT);
+	for(uint col = 0; col < COL_NUM; col++){
+		pinMode(colArr[col], OUTPUT);
+		digitalWrite(colArr[col], LOW);
+		for(uint row = 0; row < ROW_NUM; row++){
+			numpadStateLast[col][row] = numpadState[col][row];
+			numpadState[col][row] = !digitalRead(rowArr[row]);
 		}
+		digitalWrite(colArr[col], HIGH);
+		pinMode(colArr[col], INPUT);
 	}
 }
