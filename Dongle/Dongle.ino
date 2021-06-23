75a6959c7eee302891dc94df797406a7ca015ad0
dogeThinkpad <dogessbm@gmail.com>
2021-06-23 13:54:48 -0400

    Automatic commit.

--- Dongle/Dongle.ino
+++ Dongle/Dongle.ino
@@ -1,32 +1,28 @@
 #include        <NRFLite.h>
 #include        "rx.h"
-#define LED     LED_BUILTIN
-
-#define CE      0
-#define CSN     10
-#define TXID    1           // transmitter id (Numpad)
-#define RXID    0           // receiver id (Dongle)
-
-NRFLite radio;
 
 void setup()
 {
-    pinMode(LED, OUTPUT);
-    digitalWrite(LED, LOW);
-    radio.init(RXID, CE, CSN); // Id=0
-    Serial.begin(9600);
-    delay(1000);
-
-    Serial.println("Dongle online...");
+    rxInit();
 }
 
 void loop()
 {
-    uint8_t data;
-
+    uint8_t data[16] = {0};
+    u8 size = 0;
+    while(!radio.hasData());
     while(radio.hasData()){
-        radio.readData(&data);
-        Serial.println(data);
-        digitalWrite(LED, data!=0);
+        radio.readData(data+size);
+        size++;
     }
+
+    Serial.print("Key: ");
+    Serial.print(code[data[1]][data[2]]);
+    Serial.println(data[0]?" pressed":" released");
+
+    if(data[0])
+        Keyboard.press(code[data[1]][data[2]]);
+    else
+        Keyboard.release(code[data[1]][data[2]]);
+    Keyboard.send_now();
 }
--- Dongle/rx.h
+++ Dongle/rx.h
@@ -1,7 +1,64 @@
 #pragma once
-typedef unsigned int    uint
+#define C_NUM               4
+#define XLEN                C_NUM
+
+#define R_NUM               5
+#define YLEN                R_NUM
+
+#define LED                 LED_BUILTIN
+#define CE                  17
+#define CSN                 14
+#define TXID                1       // transmitter id (Numpad)
+#define RXID                0       // receiver id (Dongle)
+
+#define K_NUM               KEY_NUM_LOCK
+#define K_7                 KEYPAD_7
+#define K_4                 KEYPAD_4
+#define K_1                 KEYPAD_1
+#define K_0                 KEYPAD_0
+
+#define K_DIV               KEYPAD_SLASH
+#define K_8                 KEYPAD_8
+#define K_5                 KEYPAD_5
+#define K_2                 KEYPAD_2
+#define K_0                 KEYPAD_0
+
+#define K_MUL               KEYPAD_ASTERIX
+#define K_9                 KEYPAD_9
+#define K_6                 KEYPAD_6
+#define K_3                 KEYPAD_3
+#define K_DOT               KEYPAD_PERIOD
+
+#define K_SUB               KEYPAD_MINUS
+#define K_ADD               KEYPAD_PLUS
+#define K_RET               KEYPAD_ENTER
+
+typedef uint8_t             u8;
+typedef unsigned int        uint;
+typedef unsigned long       ul;
+
+const char code[C_NUM][R_NUM] = {
+    {K_NUM, K_7,    K_4,    K_1,    K_0},
+    {K_DIV, K_8,    K_5,    K_2,    K_0},
+    {K_MUL, K_9,    K_6,    K_3,    K_DOT},
+    {K_SUB, K_ADD,  K_ADD,  K_RET,  K_RET}
+};
+
+const char label[C_NUM][R_NUM] = {
+    {'L','7','4','1','0'},
+    {'/','8','5','2','0'},
+    {'*','9','6','3','.'},
+    {'-','+','+','E','E'}
+};
+NRFLite radio;
 
 void rxInit(void)
 {
-    
+    pinMode(LED, OUTPUT);
+    digitalWrite(LED, LOW);
+    radio.init(RXID, CE, CSN); // Id=0
+    Serial.begin(9600);
+    delay(1000);
+    Serial.println("Dongle online...");
+    Keyboard.begin();
 }
